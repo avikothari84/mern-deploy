e84075bba61003b9643947f419977d091de64f56
Avi Kothari <avikothari@Avis-MacBook-Air.local>
2023-11-26 15:44:19 +0530

    cookies optionsx

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -13,7 +13,10 @@ module.exports.Signup = async (req, res, next) => {
         const token = createSecretToken(user._id);
         res.cookie("token", token, {
             withCredentials: true,
-            httpOnly: false,
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: 36000000,
         });
         res
             .status(201)
@@ -41,7 +44,10 @@ module.exports.Login = async (req, res, next) => {
         const token = createSecretToken(user._id);
         res.cookie("token", token, {
             withCredentials: true,
-            httpOnly: false,
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: 36000000,
         });
         res.status(201).json({ message: "User logged in successfully", success: true });
         next()
