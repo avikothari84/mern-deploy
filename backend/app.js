c76e7c525d4cfae5a733fd211ac6bb7abd76c54f
Avi Kothari <avikothari@Avis-MacBook-Air.local>
2023-11-26 14:31:44 +0530

    update backend for auth

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,36 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
 const cors = require("cors");
+const app = express();
 require("dotenv").config();
 
+const cookieParser = require("cookie-parser");
+const authRoute = require("./Routes/AuthRoute");
+
+const { MONGODB_URI, PORT } = process.env;
 // middleware
 const corsOptions = {
-    origin: ["http://localhost:3000","https://sp-enterprises-ledger.onrender.com"] 
+    origin: ["http://localhost:3000","https://sp-enterprises-ledger.onrender.com"] ,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
 }
 app.use(express.json());
 app.use(cors(corsOptions));
 
 // connect MongoDB
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-    const PORT = process.env.PORT || 8000
+mongoose.connect(MONGODB_URI).then(() => {
     app.listen(PORT, () => {
         console.log(`App is Listening on PORT ${PORT}`);
     })
 }).catch(err => {
     console.log(err);
 });
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.use("/", authRoute);
 
 // route
 app.get("/", (req, res) => {
